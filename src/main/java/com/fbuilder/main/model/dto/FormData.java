e53dd9d1f87e26f55fcefd18edd9b8c95ee7eb92
MarcpTr <marcphtr@gmail.com>
2025-06-19 15:37:10 +0200

    Create FormData.java

---
+++ src/main/java/com/fbuilder/main/model/dto/FormData.java
@@ -0,0 +1,23 @@
+package com.fbuilder.main.model.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FormData {
+    private List<String> questions;
+    private List<CheckboxGroup> checkboxes;
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class CheckboxGroup {
+        private String question;
+        private List<String> option;
+
+    }
+}
