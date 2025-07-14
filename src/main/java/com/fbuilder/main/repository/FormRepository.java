e24ec73bce43cedc87eb741f006e7713ad6ee4bf
MarcpTr <marcphtr@gmail.com>
2025-07-14 22:22:33 +0200

    updated

--- src/main/java/com/fbuilder/main/repository/FormRepository.java
+++ src/main/java/com/fbuilder/main/repository/FormRepository.java
@@ -2,11 +2,9 @@ package com.fbuilder.main.repository;
 
 import com.fbuilder.main.model.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface FormRepository extends JpaRepository<Form, Integer> {
     Optional<List<Form>> findByUserId(Long id);
-
 }
